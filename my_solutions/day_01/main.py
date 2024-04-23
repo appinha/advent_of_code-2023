30a7d5ea0339da39210d9c78e0aecc9e0e3d1558
appinha <amanda_pinha@hotmail.com>
2024-04-23 09:14:52 -0300

    Adds day 1 part 2 solution

--- my_solutions/day_01/main.py
+++ my_solutions/day_01/main.py
@@ -10,19 +10,61 @@ class DayPuzzleSolver():
     def solve_part_1(self, raw_input):
         result = 0
         for row in raw_input:
-            result += find_calibration_value(row)
+            result += find_calibration_value_p1(row)
         return result
 
     def solve_part_2(self, raw_input):
-        ...
+        result = 0
+        for row in raw_input:
+            result += find_calibration_value_p2(row)
+        return result
 
-def find_calibration_value(string):
+
+numbers = {
+    1: "one",
+    2: "two",
+    3: "three",
+    4: "four",
+    5: "five",
+    6: "six",
+    7: "seven",
+    8: "eight",
+    9: "nine",
+}
+
+
+def find_calibration_value_p1(string):
+
+    def find_calibration_digit(string):
+        for char in string:
+            if char.isdigit():
+                return char
+
     first_digit = find_calibration_digit(string)
     last_digit = find_calibration_digit(string[::-1])
     return int("{}{}".format(first_digit, last_digit))
 
 
-def find_calibration_digit(string):
-    for char in string:
-        if char.isdigit():
-            return char
+def find_calibration_value_p2(string):
+
+    def find_calibration_digit(string, is_reversed = False):
+        for i in range(0, len(string)):
+            digit = match_string_to_digit(string[i:i + 5], is_reversed)
+            if digit:
+                return digit
+
+    def match_string_to_digit(string, is_reversed):
+        for i in range(0, 2):
+            if string[i].isdigit():
+                return string[i]
+
+        if is_reversed:
+            string = string[::-1]
+        for digit, word in numbers.items():
+            if word in string:
+                return digit
+
+    first_digit = find_calibration_digit(string)
+    last_digit = find_calibration_digit(string[::-1], True)
+    return int("{}{}".format(first_digit, last_digit))
+
